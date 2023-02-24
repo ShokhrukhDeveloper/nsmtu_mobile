9a1e16bc4e5e770d5848ffd5fe10d78e644f02c5
ShohkhrukhDeveloper <shoxruxyarashovuz@gmial.com>
2023-02-24 09:04:26 +0500

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nsmtu_mobile/Presentation/Pages/Home/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,29 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Navoiy Davlat Konchilik va Texnologiylari Universeteti',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primaryColor: Colors.indigo
       ),
-      home: const Home(title: 'Navoiy Davlat Konchilik va Texnologiylari Universeteti'),
+      home: const HomePage()
     );
   }
 }
-class Home extends StatefulWidget {
-  const Home({super.key, required this.title});
-  final String title;
 
-  @override
-  State<Home> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-     // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
